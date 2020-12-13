8039fa4072a3aa53e9a3657adb1c27c80882eb2a
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-12-13 21:22:52 +0530

    LCS problem

--- DP/src/java/dp/JavaLCS.java
+++ DP/src/java/dp/JavaLCS.java
@@ -2,7 +2,16 @@ package java.dp;
 
 public class JavaLCS {
     
-	public static int lcs()
+	public static int max(int x,int y)
+	{
+         if(x >= y)
+        	 return x;
+         else
+        	 return y;
+	}
+	
+	
+	public static int lcs(String str1,String str2)
 	{
 		int num = 0; 
 		
@@ -15,6 +24,10 @@ public class JavaLCS {
           
 		String str1 = "ABCBDAB";
 		String str2 = "BDCABC";
+		
+		
+		System.out.println("LCS of given two string is : "+lcs(str1,str2));
+		
 	}
 
 }
