a8033fb95fc6ef3cfe187b0e9046aba7ff84277c
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-12-13 21:17:57 +0530

    LCS problem

---
+++ DP/src/java/dp/JavaLCS.java
@@ -0,0 +1,20 @@
+package java.dp;
+
+public class JavaLCS {
+    
+	public static int lcs()
+	{
+		int num = 0; 
+		
+		return num;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          
+		String str1 = "ABCBDAB";
+		String str2 = "BDCABC";
+	}
+
+}
