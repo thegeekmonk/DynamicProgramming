ef0850ddc129ff77cb9cbc6b656de24778596dc7
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-12-21 22:04:06 +0530

    LCS Dynamic Programming

--- DP/src/dynamic/programming/java/JavaLCS.java
+++ DP/src/dynamic/programming/java/JavaLCS.java
@@ -30,7 +30,7 @@ public class JavaLCS {
 			{
 				if(str1.charAt(i) == str2.charAt(j))
 					if(i == 0 || j == 0)
-						D[i][j] = 0;
+						D[i][j] = 1;
 					else
 					    D[i][j] = D[i-1][j-1] + 1;
 				else
