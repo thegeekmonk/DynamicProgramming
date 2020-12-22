8cfb11555290e24078a6d150cad385fb913984b0
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2020-12-22 07:54:00 +0530

    partial correct result

--- DP/src/dynamic/programming/java/JavaLCS.java
+++ DP/src/dynamic/programming/java/JavaLCS.java
@@ -18,25 +18,31 @@ public class JavaLCS {
 		int len1 = str1.length();
 		int len2 = str2.length();
 		
-		//solving the base case
-		for(int i = 0;i < len1;i++)
+		//solving the base case for 1st string
+		for(int i = 0;i <= len1;i++)
 			D[i][0] = 0;
 		
-		//solving the base case
-		for(int j = 0;j < len2;j++)
+		//solving the base case for 2nd string
+		for(int j = 0;j <= len2;j++)
 			D[j][0] = 0;
 		
-		for(int i = 1;i < len1;i++)
+		
+//		if(str1.charAt(0) == str2.charAt(0))
+//			   D[0][0] =  1;	
+		
+		
+		for(int i = 1;i <= len1;i++)
 		{
-			for(int j = 1;j < len2;j++)
+			for(int j = 1;j <= len2;j++)
 			{
-				if(str1.charAt(i) == str2.charAt(j))
-					D[i][j] = D[i-1][j-1] + 1;
+				if(str1.charAt(i-1) == str2.charAt(j-1))
+				{	
+					   D[i][j] = D[i-1][j-1] + 1;
+				}	
 				else
 				{   
 					D[i][j] = max(D[i-1][j],D[i][j-1]);
-				}
-					
+				}					
 			}
 		}
 		
@@ -50,8 +56,7 @@ public class JavaLCS {
 		String str1 = "ABCBDAB";
 		String str2 = "BDCABC";
 				
-		System.out.println("LCS of given two string is : "+lcs(str1,str2));
-		
+		System.out.println("LCS of given two string is : "+lcs(str1,str2));		
 	}
 
 }
