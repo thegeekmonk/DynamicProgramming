a329dd248a289b45bd72b8784595bced8b7f3704
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-01-01 23:46:33 +0530

    @Stanford Make string palindrome

--- DP/src/dynamic/programming/java/MakeStringPalindrome.java
+++ DP/src/dynamic/programming/java/MakeStringPalindrome.java
@@ -1,14 +1,26 @@
 package dynamic.programming.java;
 
+import java.util.HashMap;
+
 public class MakeStringPalindrome {
     
 	private int count = 0;
 	
 	public int insertMinCharToPalindrome(String str)
 	{   
-		int len = str.length();
+		int len = str.length();		
+		int mid = len/2;
+		
+		HashMap<Character,Integer> map = new HashMap<Character,Integer>();
 		
 		
+		
+		if(len%2 == 0)
+		 for(int i = 0;i < mid;i++)
+		 {}
+		else
+		 for(int j = 0;j <= mid;j++)
+		 {}
 		
 		return count;
 	}
