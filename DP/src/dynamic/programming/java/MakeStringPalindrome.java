258df7a4412f531b0b241718e30f4bcd10733949
Akhilesh Kumar <kumarakhilesh5441@gmail.com>
2021-01-02 09:29:38 +0530

    @Stanford Make string palindrome

--- DP/src/dynamic/programming/java/MakeStringPalindrome.java
+++ DP/src/dynamic/programming/java/MakeStringPalindrome.java
@@ -11,16 +11,23 @@ public class MakeStringPalindrome {
 		int len = str.length();		
 		int mid = len/2;
 		
-		HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+		HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
 		
 		
-		
+		//store first half of string into hashmap
 		if(len%2 == 0)
 		 for(int i = 0;i < mid;i++)
-		 {}
+		 {
+			hm.put(str.charAt(i),i); 
+		 }
 		else
 		 for(int j = 0;j <= mid;j++)
-		 {}
+		 {
+			 hm.put(str.charAt(j),j); 
+		 }
+		
+		
+		
 		
 		return count;
 	}
